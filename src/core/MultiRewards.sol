50b544746e2946da72b439414b9e99cd8fec030c
sandybradley <sandy.bradley@gmail.com>
2025-01-15 13:22:51 +0200

    feat: avoid reward token bomb attack

--- src/core/MultiRewards.sol
+++ src/core/MultiRewards.sol
@@ -233,7 +233,9 @@ abstract contract MultiRewards is ReentrancyGuard, Pausable, IMultiRewards {
             address _rewardsToken = rewardTokens[i];
             uint256 reward = rewards[_user][_rewardsToken];
             if (reward > 0) {
-                (bool success, bytes memory data) = _rewardsToken.call(
+                (bool success, bytes memory data) = _rewardsToken.call{
+                    gas: 200000
+                }(
                     abi.encodeWithSelector(
                         ERC20.transfer.selector, _user, reward
                     )
