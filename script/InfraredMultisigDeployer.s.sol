af9e24b6514ec8360ce346e04fb693ba132fbbec
sandybradley <sandy.bradley@gmail.com>
2025-01-18 11:07:41 +0200

    chore: resolve stack too deep in multisig deployer

--- script/InfraredMultisigDeployer.s.sol
+++ script/InfraredMultisigDeployer.s.sol
@@ -26,7 +26,23 @@ import {InfraredBERAWithdraworLite} from
 import {InfraredBERAFeeReceivor} from "src/staking/InfraredBERAFeeReceivor.sol";
 import {InfraredBERAConstants} from "src/staking/InfraredBERAConstants.sol";
 
-contract InfraredDeployer is BatchScript {
+contract InfraredMultisigDeployer is BatchScript {
+    uint256 nonce;
+    address proxyInfrared;
+    address proxyCollector;
+    address proxyDistributor;
+    address proxyIberaDepositor;
+    address proxyIbera;
+    address proxyIberaWithdrawor;
+    address proxyIberaReceivor;
+    address proxyVoter;
+    address ibgt;
+    address red;
+    address veIRED;
+    bytes proxyBytecode;
+    bytes initializeData;
+    Infrared.InitializationData data;
+
     function run(
         address _gov,
         address _keeper,
@@ -40,159 +56,258 @@ contract InfraredDeployer is BatchScript {
         uint256 _bribeCollectorPayoutAmount
     ) external isBatch(_gov) {
         // Start with nonce tracking for address computation
-        uint256 nonce = vm.getNonce(_gov);
+        nonce = vm.getNonce(_gov);
 
+        // deploy contracts and proxies
+        deployInfrared(_gov);
+        deployBribeCollector(_gov, _wbera, _bribeCollectorPayoutAmount);
+        deployDistributor(_gov);
+        deployInfraredBera(_gov, _wbera);
+        deployInfraredBeraDepositor(_gov);
+        deployInfraredBeraWithdrawor(_gov);
+        deployInfraredBeraReceivor(_gov);
+        deployVoter(_gov);
+
+        // initialize proxies
+        initializeCollector(_gov, _wbera, _bribeCollectorPayoutAmount);
+        initializeDistributor(_gov);
+        initializeInfrared(
+            _gov,
+            _keeper,
+            _bgt,
+            _berachainRewardsFactory,
+            _beraChef,
+            _wbera,
+            _honey,
+            _rewardsDuration
+        );
+
+        // deploy tokens
+        deployIbgt(_gov, _bgt);
+        deployRed(_gov);
+
+        // set tokens
+
+        // infrared.setIBGT(address(ibgt));
+        addToBatch(
+            proxyInfrared,
+            0,
+            abi.encodeWithSelector(Infrared.setIBGT.selector, ibgt)
+        );
+
+        // infrared.setRed(address(red));
+        addToBatch(
+            proxyInfrared,
+            0,
+            abi.encodeWithSelector(Infrared.setRed.selector, red)
+        );
+
+        deployVeIred(_gov, _keeper);
+
+        initializeVoter(_gov, _keeper);
+
+        // initialize ibera proxies
+        initializeIberaDepositor(_gov, _keeper, _beaconDeposit);
+        initializeIberaWithdrawor(_gov, _keeper);
+        initializeIberaReceivor(_gov, _keeper);
+        initializeIbera(_gov, _keeper);
+
+        // Execute the batch
+        vm.startBroadcast();
+        executeBatch(true); // Execute all at once
+        vm.stopBroadcast();
+    }
+
+    function deployInfrared(address _gov) internal {
         // infrared = Infrared(payable(setupProxy(address(new Infrared()))));
 
         // Step 1: Deploy `Infrared` implementation
-        address infraredImpl = computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, type(Infrared).creationCode); // Empty `to`, deployment bytecode in `data`
 
         // Step 2: Deploy `ERC1967Proxy` linked to `Infrared`
-        bytes memory proxyBytecode = abi.encodePacked(
+        proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(infraredImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyInfrared = computeCreateAddress(_gov, nonce++);
+        proxyInfrared = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
+    function deployBribeCollector(
+        address _gov,
+        address _wbera,
+        uint256 _bribeCollectorPayoutAmount
+    ) internal {
         // collector = BribeCollector(
         //     setupProxy(address(new BribeCollector(address(infrared))))
         // );
 
         // Batch deploy `BribeCollector`
-        bytes memory collectorBytecode = abi.encodePacked(
-            type(BribeCollector).creationCode, abi.encode(proxyInfrared)
+        addToBatch(
+            address(0),
+            0,
+            abi.encodePacked(
+                type(BribeCollector).creationCode, abi.encode(proxyInfrared)
+            )
         );
-        addToBatch(address(0), 0, collectorBytecode);
-        address collectorImpl = computeCreateAddress(_gov, nonce++);
 
         // Deploy `BribeCollector` and set up proxy
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(collectorImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyCollector = computeCreateAddress(_gov, nonce++);
+        proxyCollector = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
+    function deployDistributor(address _gov) internal {
         // distributor = InfraredDistributor(
         //     setupProxy(address(new InfraredDistributor(address(infrared))))
         // );
 
         // Batch deploy `Distributor`
-        bytes memory distributorBytecode = abi.encodePacked(
-            type(InfraredDistributor).creationCode, abi.encode(proxyInfrared)
+        addToBatch(
+            address(0),
+            0,
+            abi.encodePacked(
+                type(InfraredDistributor).creationCode,
+                abi.encode(proxyInfrared)
+            )
         );
-        addToBatch(address(0), 0, distributorBytecode);
-        address distributorImpl = computeCreateAddress(_gov, nonce++);
 
         // Deploy `BribeCollector` and set up proxy
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(distributorImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyDistributor = computeCreateAddress(_gov, nonce++);
+        proxyDistributor = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
-        // // InfraredBERA
+    function deployInfraredBera(address _gov, address _wbera) internal {
+        // InfraredBERA
         // ibera = InfraredBERA(setupProxy(address(new InfraredBERA())));
 
         // Deploy `InfraredBERA` implementation
-        address iberaImpl = computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, type(InfraredBERA).creationCode); // Empty `to`, deployment bytecode in `data`
 
         // Deploy `ERC1967Proxy` linked to `InfraredBERA`
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(iberaImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyIbera = computeCreateAddress(_gov, nonce++);
+        proxyIbera = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
+    function deployInfraredBeraDepositor(address _gov) internal {
         // depositor = InfraredBERADepositor(
         //     setupProxy(address(new InfraredBERADepositor()))
         // );
 
         // Deploy `InfraredBERADepositor` implementation
-        address depositorImpl = computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, type(InfraredBERADepositor).creationCode); // Empty `to`, deployment bytecode in `data`
 
         // Deploy `ERC1967Proxy` linked to `InfraredBERADepositor`
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(depositorImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyIberaDepositor = computeCreateAddress(_gov, nonce++);
+        proxyIberaDepositor = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
+    function deployInfraredBeraWithdrawor(address _gov) internal {
         // withdrawor = InfraredBERAWithdraworLite(
         //     payable(setupProxy(address(new InfraredBERAWithdraworLite())))
         // );
 
         // Deploy `InfraredBERAWithdraworLite` implementation
-        address withdraworImpl = computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, type(InfraredBERAWithdraworLite).creationCode); // Empty `to`, deployment bytecode in `data`
 
         // Deploy `ERC1967Proxy` linked to `InfraredBERAWithdraworLite`
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(withdraworImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyIberaWithdrawor = computeCreateAddress(_gov, nonce++);
+        proxyIberaWithdrawor = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
+    function deployInfraredBeraReceivor(address _gov) internal {
         // receivor = InfraredBERAFeeReceivor(
         //     payable(setupProxy(address(new InfraredBERAFeeReceivor())))
         // );
 
         // Deploy `InfraredBERAFeeReceivor` implementation
-        address receivorImpl = computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, type(InfraredBERAFeeReceivor).creationCode); // Empty `to`, deployment bytecode in `data`
 
         // Deploy `ERC1967Proxy` linked to `InfraredBERAFeeReceivor`
         proxyBytecode = abi.encodePacked(
             type(ERC1967Proxy).creationCode,
-            abi.encode(receivorImpl, "") // Constructor args: implementation and optional init data
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
         );
-        address proxyIberaReceivor = computeCreateAddress(_gov, nonce++);
+        proxyIberaReceivor = vm.computeCreateAddress(_gov, nonce++);
         addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
 
-        // initialize proxies
+    function deployVoter(address _gov) internal {
+        // voter = Voter(setupProxy(address(new Voter(address(infrared)))));
+
+        // Deploy Voter
+        addToBatch(
+            address(0),
+            0,
+            abi.encodePacked(
+                type(Voter).creationCode, abi.encode(proxyInfrared)
+            )
+        );
+
+        // Deploy `ERC1967Proxy` linked to `Voter`
+        proxyBytecode = abi.encodePacked(
+            type(ERC1967Proxy).creationCode,
+            abi.encode(vm.computeCreateAddress(_gov, nonce++), "") // Constructor args: implementation and optional init data
+        );
+        proxyVoter = vm.computeCreateAddress(_gov, nonce++);
+        addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
+    }
+
+    function initializeCollector(
+        address _gov,
+        address _wbera,
+        uint256 _bribeCollectorPayoutAmount
+    ) internal {
         // collector.initialize(_gov, _wbera, _bribeCollectorPayoutAmount);
 
-        bytes memory initializeData = abi.encodeWithSelector(
+        initializeData = abi.encodeWithSelector(
             BribeCollector.initialize.selector,
             _gov,
             _wbera,
             _bribeCollectorPayoutAmount
         );
         addToBatch(proxyCollector, 0, initializeData);
+    }
 
+    function initializeDistributor(address _gov) internal {
         // distributor.initialize(_gov, address(ibera));
 
         initializeData = abi.encodeWithSelector(
             InfraredDistributor.initialize.selector, _gov, proxyIbera
         );
         addToBatch(proxyDistributor, 0, initializeData);
+    }
 
-        // voter = Voter(setupProxy(address(new Voter(address(infrared)))));
-
-        // Deploy Voter
-        bytes memory voterBytecode = abi.encodePacked(
-            type(Voter).creationCode, abi.encode(proxyInfrared)
-        );
-        addToBatch(address(0), 0, voterBytecode);
-        address voterImpl = computeCreateAddress(_gov, nonce++);
-
-        // Deploy `ERC1967Proxy` linked to `Voter`
-        proxyBytecode = abi.encodePacked(
-            type(ERC1967Proxy).creationCode,
-            abi.encode(voterImpl, "") // Constructor args: implementation and optional init data
-        );
-        address proxyVoter = computeCreateAddress(_gov, nonce++);
-        addToBatch(address(0), 0, proxyBytecode); // Empty `to`, deployment bytecode in `data`
-
-        Infrared.InitializationData memory data = Infrared.InitializationData(
+    function initializeInfrared(
+        address _gov,
+        address _keeper,
+        address _bgt,
+        address _berachainRewardsFactory,
+        address _beraChef,
+        address _wbera,
+        address _honey,
+        uint256 _rewardsDuration
+    ) internal {
+        data = Infrared.InitializationData(
             _gov,
             _keeper,
             _bgt,
@@ -214,57 +329,60 @@ contract InfraredDeployer is BatchScript {
             0,
             abi.encodeWithSelector(Infrared.initialize.selector, data)
         );
+    }
 
+    function deployIbgt(address _gov, address _bgt) internal {
         // ibgt = new InfraredBGT(
         //     address(_bgt), data._gov, address(infrared), data._gov
         // );
 
         // Batch deploy `InfraredBGT`
-        bytes memory ibgtBytecode = abi.encodePacked(
-            type(InfraredBGT).creationCode,
-            abi.encode(address(_bgt), _gov, proxyInfrared, _gov)
+        addToBatch(
+            address(0),
+            0,
+            abi.encodePacked(
+                type(InfraredBGT).creationCode,
+                abi.encode(_bgt, _gov, proxyInfrared, _gov)
+            )
         );
-        addToBatch(address(0), 0, ibgtBytecode);
-        address ibgt = computeCreateAddress(_gov, nonce++);
+        ibgt = vm.computeCreateAddress(_gov, nonce++);
+    }
 
+    function deployRed(address _gov) internal {
         // red = new RED(
         //     address(ibgt), address(infrared), data._gov, data._gov, data._gov
         // );
 
         // Batch deploy `RED`
-        bytes memory redBytecode = abi.encodePacked(
-            type(InfraredBGT).creationCode,
-            abi.encode(ibgt, proxyInfrared, _gov, _gov, _gov)
-        );
-        addToBatch(address(0), 0, redBytecode);
-        address red = computeCreateAddress(_gov, nonce++);
-
-        // infrared.setIBGT(address(ibgt));
         addToBatch(
-            proxyInfrared,
+            address(0),
             0,
-            abi.encodeWithSelector(Infrared.setIBGT.selector, ibgt)
+            abi.encodePacked(
+                type(InfraredBGT).creationCode,
+                abi.encode(ibgt, proxyInfrared, _gov, _gov, _gov)
+            )
         );
+        red = vm.computeCreateAddress(_gov, nonce++);
+    }
 
-        // infrared.setRed(address(red));
-        addToBatch(
-            proxyInfrared,
-            0,
-            abi.encodeWithSelector(Infrared.setRed.selector, red)
-        );
-
+    function deployVeIred(address _gov, address _keeper) internal {
         // veIRED = new VotingEscrow(
         //     _keeper, address(red), address(voter), address(infrared)
         // );
 
         // Deploy VotingEscrow
-        bytes memory veIREDBytecode = abi.encodePacked(
-            type(VotingEscrow).creationCode,
-            abi.encode(_keeper, red, proxyVoter, proxyInfrared)
+        addToBatch(
+            address(0),
+            0,
+            abi.encodePacked(
+                type(VotingEscrow).creationCode,
+                abi.encode(_keeper, red, proxyVoter, proxyInfrared)
+            )
         );
-        addToBatch(address(0), 0, veIREDBytecode);
-        address veIRED = computeCreateAddress(_gov, nonce++);
+        veIRED = vm.computeCreateAddress(_gov, nonce++);
+    }
 
+    function initializeVoter(address _gov, address _keeper) internal {
         // voter.initialize(address(veIRED), data._gov, data._keeper);
 
         addToBatch(
@@ -274,8 +392,13 @@ contract InfraredDeployer is BatchScript {
                 Voter.initialize.selector, veIRED, _gov, _keeper
             )
         );
+    }
 
-        // // initialize ibera proxies
+    function initializeIberaDepositor(
+        address _gov,
+        address _keeper,
+        address _beaconDeposit
+    ) internal {
         // depositor.initialize(_gov, _keeper, address(ibera), _beaconDeposit);
 
         addToBatch(
@@ -289,7 +412,11 @@ contract InfraredDeployer is BatchScript {
                 _beaconDeposit
             )
         );
+    }
 
+    function initializeIberaWithdrawor(address _gov, address _keeper)
+        internal
+    {
         // withdrawor.initialize(_gov, _keeper, address(ibera));
 
         addToBatch(
@@ -302,7 +429,9 @@ contract InfraredDeployer is BatchScript {
                 proxyIbera
             )
         );
+    }
 
+    function initializeIberaReceivor(address _gov, address _keeper) internal {
         // receivor.initialize(_gov, _keeper, address(ibera), address(infrared));
 
         addToBatch(
@@ -316,10 +445,12 @@ contract InfraredDeployer is BatchScript {
                 proxyInfrared
             )
         );
+    }
 
+    function initializeIbera(address _gov, address _keeper) internal {
         // init deposit to avoid inflation attack
-        uint256 _value = InfraredBERAConstants.MINIMUM_DEPOSIT
-            + InfraredBERAConstants.MINIMUM_DEPOSIT_FEE;
+        // uint256 _value = InfraredBERAConstants.MINIMUM_DEPOSIT
+        //     + InfraredBERAConstants.MINIMUM_DEPOSIT_FEE;
 
         // ibera.initialize{value: _value}(
         //     _gov,
@@ -332,7 +463,8 @@ contract InfraredDeployer is BatchScript {
 
         addToBatch(
             proxyIbera,
-            _value,
+            InfraredBERAConstants.MINIMUM_DEPOSIT
+                + InfraredBERAConstants.MINIMUM_DEPOSIT_FEE,
             abi.encodeWithSelector(
                 InfraredBERA.initialize.selector,
                 _gov,
@@ -343,17 +475,5 @@ contract InfraredDeployer is BatchScript {
                 proxyIberaReceivor
             )
         );
-
-        // Execute the batch
-        vm.startBroadcast();
-        executeBatch(true); // Execute all at once
-        vm.stopBroadcast();
-    }
-
-    function setupProxy(address implementation)
-        internal
-        returns (address proxy)
-    {
-        proxy = address(new ERC1967Proxy(implementation, ""));
     }
 }
