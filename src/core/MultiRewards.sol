eb8b29fdb84d293cce0da85c15133af05414d15d
ControlCplusControlV <randomlikeabledude@gmail.com>
2025-01-20 10:41:32 -0500

    Fixed malicious reward rate extension issue

--- src/core/MultiRewards.sol
+++ src/core/MultiRewards.sol
@@ -8,6 +8,8 @@ import {ReentrancyGuard} from
     "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+import {Errors} from "src/utils/Errors.sol"; 
+
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
 
@@ -322,8 +324,14 @@ abstract contract MultiRewards is ReentrancyGuard, Pausable, IMultiRewards {
 
             // Remove residual before setting rate
             totalAmount = totalAmount - rewardData[_rewardsToken].rewardResidual;
-            rewardData[_rewardsToken].rewardRate =
-                totalAmount / rewardData[_rewardsToken].rewardsDuration;
+
+            uint256 oldRewardRate = rewardData[_rewardsToken].rewardRate;
+            uint256 newRewardRate = totalAmount / rewardData[_rewardsToken].rewardsDuration;
+            if (newRewardRate < oldRewardRate) {
+                revert Errors.RewardRateDecreased(); 
+            } else {
+                rewardData[_rewardsToken].rewardRate = newRewardRate;
+            }
         }
 
         rewardData[_rewardsToken].lastUpdateTime = block.timestamp;
